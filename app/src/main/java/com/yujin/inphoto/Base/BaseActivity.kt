976793e2cc6ee265ee228b5c0ddf1162666e0268
wishJinit <wishjinit@gmail.com>
2019-11-08 00:02:09 +0900

    [Feat] : Base View/ViewModel

---
+++ app/src/main/java/com/yujin/inphoto/Base/BaseActivity.kt
@@ -0,0 +1,27 @@
+package com.yujin.inphoto.Base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<viewDataBinding : ViewDataBinding, baseViewModel : BaseViewModel> : AppCompatActivity() {
+
+    lateinit var viewDataBinding: viewDataBinding
+    abstract var baseViewModel: baseViewModel
+    abstract val layoutResourceId: Int
+
+    abstract fun initSetting()
+    abstract fun setDataBinding()
+    abstract fun afterDataBinding()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewDataBinding = DataBindingUtil.setContentView(this, layoutResourceId)
+
+        initSetting()
+        setDataBinding()
+        afterDataBinding()
+    }
+}
---
+++ app/src/main/java/com/yujin/inphoto/Base/BaseViewModel.kt
@@ -0,0 +1,7 @@
+package com.yujin.inphoto.Base
+
+import androidx.lifecycle.ViewModel
+
+open class BaseViewModel: ViewModel(){
+
+}
