1327dfe494cd103c1c7cb08c3611ebe4ad04365f
wishJinit <wishjinit@gmail.com>
2019-11-09 23:02:22 +0900

    [Feat] : 로그인, 회원가입 Activity 추가

---
+++ app/src/main/java/com/yujin/inphoto/View/SignupActivity.kt
@@ -0,0 +1,13 @@
+package com.yujin.inphoto.View
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.yujin.inphoto.R
+
+class SignupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
