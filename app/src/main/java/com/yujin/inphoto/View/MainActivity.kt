7f7c91b288899329fa9b05a2d39dd609c7bcc36b
wishJinit <wishjinit@gmail.com>
2019-11-18 18:30:58 +0900

    [Feat] : MainActivity MVVM

--- app/src/main/java/com/yujin/inphoto/View/MainActivity.kt
+++ app/src/main/java/com/yujin/inphoto/View/MainActivity.kt
@@ -1,13 +1,38 @@
 package com.yujin.inphoto.view
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.content.Intent
+import android.view.View
+import androidx.lifecycle.ViewModelProviders
+import com.yujin.inphoto.Base.BaseActivity
 import com.yujin.inphoto.R
+import com.yujin.inphoto.ViewModel.MemberViewModel
+import com.yujin.inphoto.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding, MemberViewModel>() {
+    override lateinit var viewModel: MemberViewModel
+    override val layoutResourceId: Int
+        get() = R.layout.activity_main
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    override fun onDestroy() {
+        viewModel.autoSignOut(this)
+        super.onDestroy()
     }
+
+    override fun initSetting() {
+        viewModel = ViewModelProviders.of(this)[MemberViewModel::class.java]
+    }
+
+    override fun setDataBinding() {
+        viewDataBinding.activity = this
+    }
+
+    override fun afterDataBinding() {
+    }
+
+    fun clickMenuBtn(v:View) {
+        when (v.id) {
+        }
+    }
+
 }
