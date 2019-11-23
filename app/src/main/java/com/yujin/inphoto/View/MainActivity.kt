0d50db09fcb07a13c20f7477f2afbc6a6681e441
wishJinit <wishjinit@gmail.com>
2019-11-24 06:57:04 +0900

    [Feat] : 기본 Fragment 지정
    - DiaryFragment

--- app/src/main/java/com/yujin/inphoto/View/MainActivity.kt
+++ app/src/main/java/com/yujin/inphoto/View/MainActivity.kt
@@ -2,11 +2,13 @@ package com.yujin.inphoto.view
 
 import android.content.Intent
 import android.view.View
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.yujin.inphoto.Base.BaseActivity
 import com.yujin.inphoto.R
 import com.yujin.inphoto.ViewModel.MemberViewModel
 import com.yujin.inphoto.databinding.ActivityMainBinding
+import com.yujin.inphoto.view.fragment.DiaryFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity<ActivityMainBinding, MemberViewModel>() {
@@ -21,6 +23,7 @@ class MainActivity : BaseActivity<ActivityMainBinding, MemberViewModel>() {
 
     override fun initSetting() {
         viewModel = ViewModelProviders.of(this)[MemberViewModel::class.java]
+        changeContentView(DiaryFragment())
     }
 
     override fun setDataBinding() {
@@ -42,4 +45,10 @@ class MainActivity : BaseActivity<ActivityMainBinding, MemberViewModel>() {
         startActivity(intent)
         finish()
      }
+
+    private fun changeContentView(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.contents_view_main, fragment)
+        transaction.commit()
+    }
 }
