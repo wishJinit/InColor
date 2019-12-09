a3056a98b0ae9ab31366abeb296e2048877c229f
wishJinit <wishjinit@gmail.com>
2019-12-09 18:16:46 +0900

    [Feat] : 일기 상세 layout

---
+++ app/src/main/java/com/yujin/inphoto/View/DiaryActivity.kt
@@ -0,0 +1,13 @@
+package com.yujin.inphoto.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yujin.inphoto.R
+
+class DiaryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diary)
+    }
+}
