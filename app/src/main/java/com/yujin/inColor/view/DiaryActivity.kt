ba6d9e84771c5a2f8d49d2aaf7437add160380ed
wishJinit <wishjinit@gmail.com>
2020-06-11 00:39:00 +0900

    [Feat] : 선택 날짜 일기 Activity

--- app/src/main/java/com/yujin/inColor/view/DiaryActivity.kt
+++ app/src/main/java/com/yujin/inColor/view/DiaryActivity.kt
@@ -1,13 +1,33 @@
 package com.yujin.inColor.view
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import com.yujin.inColor.R
+import com.yujin.inColor.base.BaseActivity
+import com.yujin.inColor.databinding.ActivityDiaryBinding
+import com.yujin.inColor.model.vo.DiaryVO
+import com.yujin.inColor.viewModel.DiaryViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class DiaryActivity : AppCompatActivity() {
+class DiaryActivity : BaseActivity<ActivityDiaryBinding, DiaryViewModel>() {
+    override val viewModel by viewModel<DiaryViewModel>()
+    override val layoutResourceId: Int
+        get() = R.layout.activity_diary
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_diary)
+    override fun initSetting() {}
+
+    override fun setDataBinding() {
+        viewDataBinding.let {
+            it.lifecycleOwner = this
+            it.viewModel = viewModel
+        }
+    }
+
+    override fun afterDataBinding() {
+        intent.getParcelableExtra<DiaryVO>(DIARY)?.let{
+            viewModel.setDiary(it)
+        }
+    }
+
+    companion object {
+        const val DIARY = "diary"
     }
 }
